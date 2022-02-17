b348c5f2ce650c3f9bc3fc68ef349ff56584fa25
mari <63924379+Cookieglue@users.noreply.github.com>
2022-02-17 15:48:43 +0000

    Started new match functionality

--- Main.java
+++ Main.java
@@ -18,35 +18,62 @@ class Main {
         new Player("Temmie",6),
         new Player("bob",7),   
     };
+    static Player[] medway = {
+          new Player("Mark",1),
+          new Player("Mario",2),
+          new Player("Moo",3),
+          new Player("Man",4),
+          new Player("Mar dog",5),
+          new Player("Marco",6),
+          new Player("not mari",7),   
+      };
     
     public static void main(String[] args) {
         
-        Player[][] teams = {holliston, franklin};
-        matchPlayers(teams, 7, 1);
+        Player[][] teams = {holliston, franklin, medway};
+        matchPlayers(teams, 7, 2);
       
     }
     public static void matchPlayers(Player[][] teams, int playerNum, int matchNum){
-      
-        //for teams
-        for(int i = 0 ; i < teams.length ; i++){
-            //for players
-            for (int n = 0 ; n < playerNum ; n++){
-                
+
+      //for matches
+      for (int m = 0 ; m < matchNum ; m++){
+          //for teams
+          for(int i = 0 ; i < teams.length ; i++){
+              //for players
+              for (int n = 0 ; n < playerNum ; n++){
+                  
                 Player self = teams[i][n];
-                Player opponent = teams[(i+1)%2][n];
-                
-                if (self.matches[0] == null && opponent.matches[0] == null){
-                    //Set "Home" match
-                    self.setMatch(opponent, 0);
-                    //Set "Away" match
-                    opponent.setMatch(self, 0);
-                    //for matches
-                    System.out.println( self.name + " VS " + self.matches[0].name );
+
+                //for opponent teams
+                for (int op = 0 ; op < playerNum ; op++){
+                  Player opponent = teams[(op)%teams.length][n];
+                  
+                  if (self.matches[m] == null && opponent.matches[m] == null){
+                      //Set "Home" match
+                      self.setMatch(opponent, m);
+                      //Set "Away" match
+                      opponent.setMatch(self, m);
+                      //for matches
+                      System.out.println( self.name + " VS " + self.matches[0].name );
+                  }
                 }
-        
-
-            }
+  
+              }
+          }
         }
-
     }
+
+  public Player getOpponent(int matchNum, int team){
+    for (int op = 0 ; op < playerNum ; op++){
+      
+      Player opponent = teams[(op)%teams.length][n];
+      //ensure that the opponent is free and is not on the same team
+      if (opponent.matches[matchNum] == null, ){
+        
+      }
+      
+    }
+  }
+    
   }
